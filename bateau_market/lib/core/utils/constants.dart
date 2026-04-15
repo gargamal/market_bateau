d4fd48c7fe57aae4a2eab4632f0da72cc7f0b8f4
danielpellier <daniel.pellier-prestataire@itsap.com>
2026-04-15 11:50:04 +0200

    add : conf file

--- bateau_market/lib/core/utils/constants.dart
+++ bateau_market/lib/core/utils/constants.dart
@@ -1,7 +1,8 @@
-const int limitShipsPagination = 15;
+const int limitShipsPagination = int.fromEnvironment('LIMIT_PAGINATION', defaultValue: 15);
+const String baseUrl = String.fromEnvironment('BASE_URL', defaultValue: 'http://192.168.1.173:8080');
+const int lapTimeCallBackend = int.fromEnvironment('LAP_TIME_BACKEND', defaultValue: 500);
 const double limitShipsRefresh = 0.9;
-const String baseUrl = 'http://192.168.1.173:8080';
-const int lapTimeCallBackend = 500;
-const citiesBoxName = 'cities_box';
-const shipsBoxName = 'ships_box';
-const settingsBoxName = 'settings_box';
+
+const String citiesBoxName = 'cities_box';
+const String shipsBoxName = 'ships_box';
+const String settingsBoxName = 'settings_box';
